0d035ab0274d8a9202f1a1f33d3186bb17816b57
boreichenko glib <boreichenkoglib@gmail.com>
2024-03-15 19:06:35 +0200

    crud

---
+++ vendor/create_comment.php
@@ -0,0 +1,12 @@
+<?php
+
+//Добавление комментария
+
+require_once '../config/connect.php';
+
+$id = $_POST['id'];
+$body = $_POST['body'];
+
+mysqli_query($connect, "INSERT INTO `comments` (`id`, `product_id`, `body`) VALUES (NULL, '$id', '$body')");
+
+header('Location: /product.php?id=' . $id);
---
+++ vendor/delete.php
@@ -0,0 +1,11 @@
+<?php
+
+//Удаление продукта
+
+require_once '../config/connect.php';
+
+$id = $_GET['id'];
+
+mysqli_query($connect, "DELETE FROM `products` WHERE `products`.`id` = '$id'");
+
+header('Location: /');
